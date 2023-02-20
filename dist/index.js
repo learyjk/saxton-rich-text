4c08d5e0cd5ebbf6c7d4e2b8928b359cfb75e59a
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2023-02-20 08:29:51 -0800

    first

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-(()=>{var e=()=>{console.log("loaded")};document.addEventListener("DOMContentLoaded",e);})();
+(()=>{var n=()=>{console.log("hi jon"),document.querySelectorAll('a[href$="[nf]"]').forEach(e=>{e.href=e.href.replace("[nf]",""),e.setAttribute("rel","nofollow")})};document.addEventListener("DOMContentLoaded",n);})();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 const init = () => {
-    console.log('loaded')
-}
+  console.log("hi jon");
+  const links = document.querySelectorAll<HTMLAnchorElement>('a[href$="[nf]"]');
 
-document.addEventListener("DOMContentLoaded", init)
+  links.forEach((link) => {
+    link.href = link.href.replace("[nf]", "");
+    link.setAttribute("rel", "nofollow");
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
